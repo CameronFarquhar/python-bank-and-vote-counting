adae87bee39335f3dc81f653cdbef13680ae0e79
CameronFarquhar <cameronfarquhar@gmail.com>
2021-01-10 14:39:15 -0500

    add the total amounts with a for loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,33 +1,23 @@
+# import modules
 import os
 import csv
 
+#create a path leading to the CSV file
 budget_csv = os.path.join('Resources','budget_data.csv')
 
-# def function_data(data):
-    
-#     total_months = str(data[0])
-
-#     total_budget = int(data[1])
-
+#open the CSV file in this project
 with open(budget_csv, "r") as csvfile:
-
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    #skip the header row
     header = next(csvreader)
-
-    months = list(csvreader)
-
-    print(len(months))
-
-    # total_budget = [1]
-    
-    # # function_data() = [0]
-
-    # for row in csvreader:
-
-    #      if total_budget > total_budget + int(row[1]):
-
-    #         total_budget = biggest
-
-    #         print(int(biggest))
+    #set a value for total to grab all the values in the 2nd row
+    total = 0
+    #determine the amount of months
+    months = len(list(csvreader))
+    #create a loop that runs through the whole file
+    for row in csvreader:
+        #grab each value in the Total column
+        total += float(row[1])
+    #print the value with squigly brackets to contain the added total
+    print(f'total: {total}')
 
