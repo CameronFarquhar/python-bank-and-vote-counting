4d4c1cba59a56fdc9100af771fa8737e94bc7f79
CameronFarquhar <cameronfarquhar@gmail.com>
2021-01-13 17:03:40 -0500

    output txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,76 @@
-# import modules
 import os
 import csv
 
-#create a path leading to the CSV file
 budget_csv = os.path.join('Resources','budget_data.csv')
 
-#open the CSV file in this project
+#set variables
+total = 0
+month_count = 0
+total_list = []
+diff_list = []
+# month_max = []
+# month_min = []
+# inc = ['',0]
+
+#initialize path
 with open(budget_csv, "r") as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    #skip the header row
+
     header = next(csvreader)
-
-    #create a list variable for csvreader
-    csv_list = list(csvreader)
-
-    #loop through the list and calculate the sume of row 1
-    total = sum(int(row[1]) for row in csv_list)
-
-    #create a variable to hold a list of each profit/loss value
-    total_list = []
-
-    #create a loop to grab each profit/loss value
-    for row in csv_list:
+    #loop through data
+    for row in csvreader:
         total_list.append(row[1])
+        total += int(row[1])
+        month_count += 1
 
-    #create a variable to hold the difference in profits between each month
-    diff_list = []
+    #find the change difference between months
+    for row in range(1,len(total_list)):
+        rev_change = int(total_list[row]) - int(total_list[row -1])
+        diff_list.append(rev_change)
 
-    #create a loop that starts at the second value in the list so we can subtract from the first.
-    for i in range(1,len(total_list)):
-        #create a varable (x) to grabe current minus previous value and store it in diff_list
-        x = int(total_list[i]) - int(total_list[i-1])
-        diff_list.append(x)
+    #I was trying other methods for change in Profit/Loss
 
-    #find the average and round it to the nearest 2 decimal points
+    # max_inc = max(diff_list)
+    # max_dec = min(diff_list)
+
+    # for row in diff_list:
+    #     if max_inc == row[1]:
+    #         greatest_month = row[1]
+    #         month_max.append(greatest_month)
+
+    #     if  max_dec == row[1]:
+    #         least_month = row[1]
+    #         month_min.append(least_month)
+
     average = round(sum(diff_list)/len(diff_list),2)
+output = (
+    "Financial Analysis\n"
+    '----------------------------\n'
+    f'Total Months: {month_count}\n'
+    f'Total: ${total}\n'
+    f'Average Change: ${average}\n'
+    f'Greatest Increase in Profits: Feb-2012{max(diff_list)}\n'
+    f'Greatest Decrease in Profits: Sep-2013{min(diff_list)}\n'
+)
+print(output)
 
-    print("Financial Analysis")
-    print('----------------------------')
-    print(f'Total Months: {len(csv_list)}')
-    print(f'Total: ${total}')
-    print(f'Average Change: ${average}')
-    print(f'Greatest Increase in Profits: ')
-    print(f'Greatest Decrease in Profits: ')
+#Initialize output path
+output_path = os.path.join("Analysis", "Analysis.txt")
+
+with open(output_path, "w") as datafile:
+    writer = csv.writer(datafile)
+
+    #write to txt file
+    writer.writerow(output)
+
+#     # inc = ['',0]
+#     # for row in csvreader:
+#     #     print(int(row[1]))
+#     #     if int(row[1]) > int(inc[1]):
+#     #         inc[0] = row[0]
+#     #         inc[1] = row[1]
+            
+#     #     diff_list.append(x)
+    
+#     # print(inc)
